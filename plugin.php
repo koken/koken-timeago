ab0f7fa673db9597d9912736e07bde25f8dd12fc
Brad Daily <brad@slideshowpro.net>
2012-10-19 13:29:40 -0500

    Init

---
+++ plugin.php
@@ -0,0 +1,24 @@
+<?php
+
+class KokenTimeago extends KokenPlugin {
+
+	function __construct()
+	{
+		$this->register_output_filter('before_closing_head', 'setup');
+	}
+
+	function setup()
+	{
+
+		$path = $this->get_path();
+		return <<<OUT
+<script src="{$path}/jquery.timeago.js"></script>
+<script>
+	$(function() {
+		$('time').timeago();
+	});
+</script>
+OUT;
+
+	}
+}
